94f47ae1ccb66173894afa5be6e884fcdd50e632
Abagail Hudson <abagailanne@gmail.com>
2019-09-01 18:38:38 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :students, only: :index
 
    get "students/:id", to: "students#show", as: "student"
-  get "students/:id/activate", to: "students#activate", as: "activate_student"
+   get "students/:id/activate", to: "students#activate", as: "activate_student"
 
 end
